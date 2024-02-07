8a535a97b179c1ec7a5edd24b0b65b4ae64d1046
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-02-07 10:21:38 -0600

    delete randomNumber function and combine it with the getComputerChoice function, delete choice variable and add it into the getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,7 +1,18 @@
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+
+
+function getComputerChoice(min,max){
+    let choice =  Math.floor(Math.random() * (max - min) + min);
+
+    if (choice === 1) {
+        let computerChoice = "rock"
+    }
+    else if (hoice === 2) {
+        let computerChoice = "paper";
+    }
+    else{
+        let computerChoice = "scissors";
+    }
+    console.log(computerChoice);
 }
-
-
 
 
