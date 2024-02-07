46e363619b91c3a3939f2f2b7773fe9cf621db40
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-02-07 10:59:55 -0600

    restart w just the randomNumber function

--- rps.js
+++ rps.js
@@ -1,18 +1,4 @@
-
-
-function getComputerChoice(min,max){
-    let choice =  Math.floor(Math.random() * (max - min) + min);
-
-    if (choice === 1) {
-        let computerChoice = "rock"
-    }
-    else if (hoice === 2) {
-        let computerChoice = "paper";
-    }
-    else{
-        let computerChoice = "scissors";
-    }
-    console.log(computerChoice);
+function randomNumber( max) {
+  return Math.floor(Math.random() * max);
 }
-
 
