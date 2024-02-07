16f364310591935b6086473d9a6b30c8dcd2154e
Amelia Gustafson <amelia.gustafson200@gmail.com>
2024-02-07 11:26:19 -0600

    add randomNuber and computerSelecor function, computerNumber variable, see commands in notebook

--- rps.js
+++ rps.js
@@ -1,5 +1,22 @@
 function randomNumber( max) {
   return Math.floor(Math.random() * max);
 }
-const computerChoice = randomNumber(3);
+const computerNumber = randomNumber(3);
+function computerSelector(computerNumber){
+    if (computerNumber === 1){
+        computerChoice = "rock";
+    }
+    else if (computerNumber ===2){
+        computerChoice = "paper";
+    }
+    else if (computerNumber === 0){
+        console.log("try again");
+    }
+    else{
+        computerChoice = "scissors";
+    }
+}
+
+
+
 
